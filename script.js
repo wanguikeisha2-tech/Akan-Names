e1ea1c41a5700def89ce17170044388a2134544f
keisha <wanguikeisha2@gmail.com>
2026-08-02 17:06:02 +0300

    Feat:working on script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function getAkanName(){
+    let birthdate = documnent.getElementById("birthdate").value;
+    let dateParts = birthdate.split("-");
+    let year = datePart[0]
+    let month = datePart[1]
+    let day = datePart[2]
+    console.log(year)
+    console.log(month)
+    console.log(day)
+}
